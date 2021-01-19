beee1682eb41318986693f03a419f581ffcd01f0
Артем Бородовой <a.borodovoi@basicgroup.ua>
2021-01-19 20:22:44 +0200

    created added notes component

--- src/store/info.js
+++ src/store/info.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase/app';
+import firebase from 'firebase/app'
 
 export default {
     state: {
@@ -13,15 +13,26 @@ export default {
         }
     },
     actions: {
+        async updateInfo({ dispatch, commit, getters }, toUpdate) {
+            try {
+                const uid = await dispatch('getUid')
+                const updateData = { ...getters.info, ...toUpdate }
+                await firebase.database().ref(`/users/${uid}/info`).update(updateData)
+                commit('setInfo', updateData)
+            } catch (e) {
+                commit('setError', e)
+                throw e
+            }
+        },
         async fetchInfo({ dispatch, commit }) {
             try {
                 const uid = await dispatch('getUid')
                 const info = (await firebase.database().ref(`/users/${uid}/info`).once('value')).val()
                 commit('setInfo', info)
             } catch (e) {
-
+                commit('setError', e)
+                throw e
             }
-
         }
     },
     getters: {
